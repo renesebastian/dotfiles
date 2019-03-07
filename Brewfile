29724eb5f58d7699adffc0b28e8bf187eb14c223
renesebastian <info@renesebastian.nl>
2019-03-07 11:39:10 +0100

    refactor: default upgrades

--- Brewfile
+++ Brewfile
@@ -13,24 +13,18 @@ brew 'node'
 brew 'hub'
 brew 'ruby-build'
 brew 'grunt-cli'
+brew 'mysql'
 
 brew 'php71'
 brew 'composer'
 
-brew 'mas'
-
 cask "1password"
 cask "dropbox"
-cask "evernote"
 cask "google-chrome"
-cask "iterm2"
 cask "sequel-pro"
 cask "sketch"
 cask "slack"
-cask "spectacle"
 cask "sublime-text"
-cask "caskroom/drivers/sonos"
 cask "docker"
-cask "google-cloud-sdk"
 
 mas 'Trello', id: 1278508951
--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -8,31 +8,31 @@
 # Run ./set-defaults.sh and you'll be good to go.
 
 # Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write -g ApplePressAndHoldEnabled -bool true
 
-# Use AirDrop over every interface. srsly this should be a default.
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+# # Use AirDrop over every interface. srsly this should be a default.
+# defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
-# Always open everything in Finder's list view. This is important.
-defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+# # Always open everything in Finder's list view. This is important.
+# defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
-# Show the ~/Library folder.
-chflags nohidden ~/Library
+# # Show the ~/Library folder.
+# chflags nohidden ~/Library
 
-# Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 2
+# # Set a really fast key repeat.
+# defaults write NSGlobalDomain KeyRepeat -int 2
 
-# Set the Finder prefs for showing a few different volumes on the Desktop.
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+# # Set the Finder prefs for showing a few different volumes on the Desktop.
+# defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+# defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# Set up Safari for development.
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+# # Set up Safari for development.
+# defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+# defaults write com.apple.Safari IncludeDevelopMenu -bool true
+# defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+# defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+# defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
-defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+# defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
 
-defaults write com.apple.dock autohide-time-modifier -float 0.5
+# defaults write com.apple.dock autohide-time-modifier -float 0.5
